dead3da0c39b084430389fe3674ce4291fc0832c
AAKASH3014 <155516301+AAKASH3014@users.noreply.github.com>
2024-08-13 13:13:06 +0530

    Add files via upload

---
+++ DS/LAB 13/Lab13_4.java
@@ -0,0 +1,21 @@
+public class Lab13_4 {
+    public static void main(String[] args) {
+
+        CircularLL cl = new CircularLL();
+        cl.insertNodeAtLast(1);
+        cl.insertNodeAtLast(2);
+        cl.insertNodeAtLast(3);
+        cl.insertNodeAtLast(4);
+        cl.insertNodeAtLast(5);
+        cl.insertNodeAtLast(6);
+        // System.out.println("count"+cl.countNode());
+        // System.out.println("hi");
+        cl.display();
+        // System.out.println("hi");
+        CircularLL cl2 = new CircularLL();
+        cl2 = cl.splitInTOHalves();
+        cl.display();
+        // System.out.println("hi");
+        cl2.display();
+    }
+}
