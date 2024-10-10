dda1bd177a83dee4526dc1e6370993f0aa5daaa8
AAKASH3014 <155516301+AAKASH3014@users.noreply.github.com>
2024-10-10 21:35:30 +0530

    Add files via upload

---
+++ todolist - Copy/todolist - Copy/server/src/utils/constants.js
@@ -0,0 +1,10 @@
+export const DB_CONNECT = "mongodb+srv://Satyajit:t9VQBeV_b2eC*nb@cluster0.imfli.mongodb.net/"
+
+export const JWT_TOKEN_SECRET = "sjafflamieakmddvad";
+
+export const statusCode = {
+    SUCCESS: 200,
+    VALIDATION_ERROR: 201,
+    UNPROCESSABLE_ENTITY: 202,
+    AUTH_ERROR: 203
+}
